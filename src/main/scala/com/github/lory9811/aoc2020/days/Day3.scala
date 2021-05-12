0647d8b867f2b602c4b90934da13bb83d89e136d
Lorenzo Manna <lorenzo.98m@gmail.com>
2021-05-12 22:10:46 +0200

    Changed Day3's Vector2 to case class.

--- src/main/scala/com/github/lory9811/aoc2020/days/Day3.scala
+++ src/main/scala/com/github/lory9811/aoc2020/days/Day3.scala
@@ -3,19 +3,19 @@ package com.github.lory9811.aoc2020.days
 object Day3 extends Day {
   override val inputIndex: Int = 3
 
-  private class Vector2(var x: Int, var y: Int) {
+  private case class Vector2(var x: Int, var y: Int) {
     def +(other: Vector2): Vector2 = {
-      new Vector2(x + other.x, y + other.y)
+      Vector2(x + other.x, y + other.y)
     }
   }
 
   private object Vector2 {
-    def apply(ints: (Int, Int)): Vector2 = new Vector2(ints._1, ints._2)
+    def apply(ints: (Int, Int)): Vector2 = Vector2(ints._1, ints._2)
   }
 
-  private class Map(mapData: Set[(Int, Int)], dimensions: Vector2) {
+  private class Map(mapData: Set[Vector2], dimensions: Vector2) {
     def testCollision(position: Vector2): Boolean = {
-      mapData.contains((position.x % dimensions.x, position.y))
+      mapData.contains(Vector2(position.x % dimensions.x, position.y))
     }
 
     def width: Int = dimensions.x
@@ -28,8 +28,8 @@ object Day3 extends Day {
           y <- input.indices
           x <- input(y).indices
           if input(y)(x) == '#'
-        } yield (x, y)).toSet,
-        new Vector2(input.head.length, input.size)
+        } yield Vector2(x, y)).toSet,
+        Vector2(input.head.length, input.size)
     )
   }
 
@@ -44,14 +44,14 @@ object Day3 extends Day {
   }
 
   override def part1(input: List[String]): Int = {
-    val slope = new Vector2(3, 1)
+    val slope = Vector2(3, 1)
     val map = Map(input)
-    getCollisions(new Vector2(0, 0), slope, map)
+    getCollisions(Vector2(0, 0), slope, map)
   }
 
   override def part2(input: List[String]): Int = {
     val slopes = Set((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)).map(Vector2(_))
     val map = Map(input)
-    (for (slope <- slopes) yield getCollisions(new Vector2(0, 0), slope, map)).product
+    (for (slope <- slopes) yield getCollisions(Vector2(0, 0), slope, map)).product
   }
 }
