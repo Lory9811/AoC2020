02f9a95d980cce5d33c0aee9b1948f7856f7a789
Lorenzo Manna <lorenzo.98m@gmail.com>
2021-05-12 15:33:40 +0200

    Day 2 part 2

--- src/main/scala/com/github/lory9811/aoc2020/Main.scala
+++ src/main/scala/com/github/lory9811/aoc2020/Main.scala
@@ -7,4 +7,5 @@ object Main extends App {
   println(s"Day1 part2: ${Day1.part2(Util.getInput(1))}")
 
   println(s"Day2 part1: ${Day2.part1(Util.getInput(2))}")
+  println(s"Day2 part2: ${Day2.part2(Util.getInput(2))}")
 }
--- src/main/scala/com/github/lory9811/aoc2020/days/Day2.scala
+++ src/main/scala/com/github/lory9811/aoc2020/days/Day2.scala
@@ -9,6 +9,13 @@ object Day2 {
     if (min to max contains count) true else false
   }
 
+  private val validatePassword2 = (input: String) => {
+    val tokens = input.split(':').map(_.strip)
+    val (pos0, pos1, letter) = parsePasswordPattern(tokens(0))
+    val password = tokens(1)
+    (password(pos0 - 1) == letter || password(pos1 - 1) == letter) && !(password(pos0 - 1) == letter && password(pos1 - 1) == letter)
+  }
+
   private val parsePasswordPattern = (pattern: String) => {
     val tokens = pattern.split("[- ]")
     (tokens(0).toInt, tokens(1).toInt, tokens(2)(0))
@@ -16,5 +23,9 @@ object Day2 {
 
   val part1: List[String] => Int = (input: List[String]) => {
     input.map(validatePassword).count(_ == true)
+  }
+
+  val part2: List[String] => Int = (input: List[String]) => {
+    input.map(validatePassword2).count(_ == true)
   }
 }
