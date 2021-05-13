6805c77b5702f5669ed2215931522ad88bc896b8
Lorenzo Manna <lorenzo.98m@gmail.com>
2021-05-13 13:32:01 +0200

    Day 5 part 2

--- src/main/scala/com/github/lory9811/aoc2020/days/Day5.scala
+++ src/main/scala/com/github/lory9811/aoc2020/days/Day5.scala
@@ -3,11 +3,6 @@ package com.github.lory9811.aoc2020.days
 object Day5 extends Day {
   override val inputIndex: Int = 5
 
-  def findPos(input: String, zero: String, one: String): Int = {
-    val binaryString = input.replace(zero, "0").replace(one, "1")
-    Integer.parseInt(binaryString, 2)
-  }
-
   def findId(input: String): Int = {
     val binaryString = input.replace("F", "0").replace("B", "1")
       .replace("L", "0").replace("R", "1")
@@ -18,5 +13,11 @@ object Day5 extends Day {
     input.map(findId).max
   }
 
-  override def part2(input: List[String]): Int = 0
+  override def part2(input: List[String]): Int = {
+    val seats = input.map(findId)
+    (for {
+      seat <- seats
+      if seats.contains(seat) && seats.contains(seat - 2) && !seats.contains(seat - 1)
+    } yield seat - 1).head
+  }
 }
